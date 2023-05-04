bd845b67d97b98249972194034695c8488a40123
Lucas Araujo <lucasaraujoo44.la@gmail.com>
2023-05-04 14:06:42 -0300

    Adicionar arquivos de projeto.

---
+++ bytebank/Program.cs
@@ -0,0 +1,97 @@
+﻿using bytebank.Contas;
+using bytebank.Titular;
+using System.Xml;
+
+//ContaCorrente contaDoAndre = new ContaCorrente();
+//contaDoAndre.titular = "André Silva";
+//contaDoAndre.numero_agencia = 12;
+//contaDoAndre.conta = "1010-x";
+//contaDoAndre.saldo = 100;
+//Console.WriteLine("O saldo da conta é = " + contaDoAndre.saldo);
+
+
+//ContaCorrente contaDoAndre2 = new ContaCorrente();
+//contaDoAndre.titular = "André Silva";
+//contaDoAndre.numero_agencia = 12;
+//contaDoAndre.conta = "1010-x";
+//contaDoAndre.saldo = 100;
+//Console.WriteLine("O saldo da conta é = " + contaDoAndre2.saldo);
+
+//Console.WriteLine(contaDoAndre ==contaDoAndre2);
+
+//Console.WriteLine("O saldo da conta é = " + contaDoAndre.saldo);
+
+//ContaCorrente contaDaMaria = new ContaCorrente();
+//contaDaMaria.titular = "maria";
+//contaDaMaria.numero_agencia = 17;
+//contaDaMaria.conta = "1010-5";
+//contaDaMaria.saldo = 350;
+
+//Console.WriteLine("O saldo da conta é = " + contaDaMaria.saldo);
+
+//contaDoAndre.Transferir(50, contaDaMaria);
+//Console.WriteLine("Saldo do André = "+contaDoAndre.saldo);
+//Console.WriteLine("Saldo da Maria = " + contaDaMaria.saldo);
+
+//ContaCorrente contaDoPedro = new ContaCorrente();
+//contaDoPedro.titular = "Pedro";
+//Console.WriteLine(contaDoPedro.titular);
+//Console.WriteLine(contaDoPedro.saldo);
+//Console.WriteLine(contaDoPedro.numero_agencia);
+//Console.WriteLine(contaDoPedro.conta);
+
+//double valor = 300;
+//double valor2=valor;
+
+//Console.WriteLine(valor ==valor2);
+//Console.WriteLine(valor);
+//Console.WriteLine(valor2);
+
+//Cliente cliente = new Cliente();
+//cliente.nome = "André Silva";
+//cliente.cpf = "123456789";
+//cliente.profissao = "Analista";
+
+//ContaCorrente conta = new ContaCorrente();
+//conta.titular = cliente;
+//conta.conta = "1010-x";
+//conta.numero_agencia = 15;
+//conta.saldo = 100;
+//Console.WriteLine("Titular = "+conta.titular.nome);
+//Console.WriteLine("Cpf = " + conta.titular.cpf);
+//Console.WriteLine("Profissao = " + conta.titular.profissao);
+//Console.WriteLine("N° conta = " + conta.conta);
+//Console.WriteLine("Saldo= " + conta.saldo);
+//Console.WriteLine("N° Agencia = " + conta.numero_agencia);
+
+//ContaCorrente conta2 = new ContaCorrente();
+//conta2.titular = new Cliente();
+//conta2.titular.nome = "Jose Souza";
+//conta2.titular.cpf = "987654321";
+//conta2.titular.profissao = "Tester";
+//conta2.conta = "9999-x";
+//conta2.numero_agencia = 18;
+//conta2.saldo = 500;
+
+//Console.WriteLine(conta2.titular.nome);
+
+//ContaCorrente conta3 = new ContaCorrente();
+//conta3.Numero_Agencia = 18;
+//conta3.Conta = "1011-h";
+//conta3.SetSaldo (200);
+//Console.WriteLine(conta3.GetSaldo());
+//Console.WriteLine(conta3.Numero_Agencia);
+//Console.WriteLine(conta3.Conta);
+
+//ContaCorrente conta4 = new ContaCorrente(18,"1010-x");
+//conta4.SetSaldo(500);
+//conta4.titular = new Cliente();
+
+//Console.WriteLine(conta4.GetSaldo());
+//Console.WriteLine(conta4.Numero_agencia);
+
+ContaCorrente conta5 = new ContaCorrente(283, "1234-x");
+Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+ContaCorrente conta6 = new ContaCorrente(284, "9874-z");
+Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
